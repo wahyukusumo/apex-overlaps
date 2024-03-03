8bdc98680eeb31ecad3464a91d11bc4bb6f1d5f9
Wahyu Kusumo <54056573+wahyukusumo@users.noreply.github.com>
2024-03-03 14:13:27 +0700

    Change base to root

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/apex-overlaps/',
+  base: '/',
   plugins: [vue()],
   server: {
     host: true,
